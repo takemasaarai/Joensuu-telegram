76f6e09c37aba373b3380d3019be480f1dd49cd7
EC2 Default User <ec2-user@ip-172-31-16-101.ap-southeast-1.compute.internal>
2019-09-27 12:00:27 +0000

    rails new

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module JoensuuTelegram
+  class Application < Rails::Application
+    
+    config.load_defaults 5.2
+
+  end
+end
