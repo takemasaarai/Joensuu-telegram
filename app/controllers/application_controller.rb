526085a7a8d4cdd542aa23629a5d87dc5b10603c
EC2 Default User <ec2-user@ip-172-31-16-101.ap-southeast-1.compute.internal>
2019-09-30 21:32:49 +0000

    Update view signup,signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
     
-    def hello
-        render html: "hello,world!"
-    end
+   protect_from_forgery with: :exception
+   before_action :configure_permitted_parameters, if: :devise_controller?
+   
+   protected
+   
+   def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+   end
 end    
 
