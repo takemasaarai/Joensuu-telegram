082979cd7d322755ded326bbb7baced01c481782
EC2 Default User <ec2-user@ip-172-31-16-101.ap-southeast-1.compute.internal>
2019-10-06 10:49:25 +0000

    Add Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
-    root 'pages#home'
     
-    get '/users/:id', to: 'users#show', as: 'user'
+    root 'posts#index'
+    
+    get  '/users/:id', to: 'users#show', as: 'user'
+    
+    resources :posts, only: %i(new create index show destroy) do
+      resources :photos, only: %i(create)
+    end
 end
